bbd872ff196ee2261d6657614d1a2fca6a684f7c
Abi Turner <turner.abi@gmail.com>
2024-07-02 20:16:15 +0100

    created CRUD functionality for User controller and passed integration testing

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,4 +4,12 @@ const userController = require("../controllers/userController");
 
 router.post("/", userController.createUser);
 
+router.get("/", userController.getUsers);
+
+router.get("/:id", userController.getUserById);
+
+router.patch("/:id", userController.updateUser);
+
+router.delete("/:id", userController.deleteUser);
+
 module.exports = router;
